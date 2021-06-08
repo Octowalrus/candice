24a59767a5ea919ba842e3afdf7c7cc6dabc5530
Octowalrus <44419504+Octowalrus@users.noreply.github.com>
2021-06-08 00:51:06 -0400

    Update run-this.sh

--- run-this.sh
+++ run-this.sh
@@ -1,5 +1,6 @@
 sudo apt update && sudo apt upgrade
-sudo apt install neofetch wget git figlet net-tools
+sudo apt install neofetch wget git figlet net-tools nginx python synaptic
 figlet laura is short
 neofetch
 uname -a
+echo "It's Done!"

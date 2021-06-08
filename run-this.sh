1f2a55a99b39ce80588667d02afadf3cfae5a015
Octowalrus <44419504+Octowalrus@users.noreply.github.com>
2021-06-08 00:52:54 -0400

    Update run-this.sh

--- run-this.sh
+++ run-this.sh
@@ -1,6 +1,7 @@
 sudo apt update && sudo apt upgrade
-sudo apt install neofetch wget git figlet net-tools nginx python synaptic
+sudo apt install neofetch wget git figlet net-tools nginx python synaptic python-pip
 figlet laura is short
 neofetch
 uname -a
+echo "This installs tools for normal linux, as well as basic web design and python."
 echo "It's Done!"

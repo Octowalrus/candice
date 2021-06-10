8daad3d1c9f39e37a53ba3ce18594b191381d86a
Octowalrus <44419504+Octowalrus@users.noreply.github.com>
2021-06-10 00:31:29 -0400

    bruh
    xd

--- run-this.sh
+++ run-this.sh
@@ -5,3 +5,5 @@ neofetch
 uname -a
 echo "This installs tools for normal linux, as well as basic web design and python."
 echo "It's Done, Love youuu (more than laura lol)"
+read -n 1 -r -s -p $'Press any key to restart...\n'
+sudo reboot

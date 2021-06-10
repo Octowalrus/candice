6a095aa9bc807ab0e3c7e8f3e3d4f7445c85884c
Octowalrus <44419504+Octowalrus@users.noreply.github.com>
2021-06-10 00:33:56 -0400

    ඞ amogus
    added python stuff

--- run-this.sh
+++ run-this.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install neofetch wget git figlet net-tools nginx python synaptic python-pip nano vim cmatrix make -y
+sudo apt install neofetch wget git figlet net-tools nginx python synaptic python-pip thonny nano vim cmatrix make -y
 figlet laura is short
 neofetch
 uname -a
